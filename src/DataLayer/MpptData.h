af5f4dc77956d3c32969fe5f8efd2af731ccea48
Jeffery <jeffhuo77@gmail.com>
2017-01-28 11:27:29 -0700

    Merge conflicts MpptData

--- src/DataLayer/MpptData.h
+++ src/DataLayer/MpptData.h
@@ -3,13 +3,16 @@
 struct MpptData
 {
     MpptData()
-        : alive(false)
+        : mpptNumber(0)
+        , alive(true)
         , arrayVoltage(71)
         , arrayCurrent(72)
         , batteryVoltage(73)
         , temperature(74)
     {}
 
+    /* has to be between 0 and 2 (inclusive) */
+    unsigned char mpptNumber;
     bool alive;
     unsigned short arrayVoltage;
     unsigned short arrayCurrent;
