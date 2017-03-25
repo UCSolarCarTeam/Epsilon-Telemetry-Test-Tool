1038adf1e4248f76faabde19d302ed5c40f0d31f
Jeffery <jeffhuo77@gmail.com>
2017-03-25 12:43:11 -0600

    Inital change to BatteryFaultsData to new protocol

--- src/DataLayer/BatteryFaultsData.h
+++ src/DataLayer/BatteryFaultsData.h
@@ -31,4 +31,58 @@ struct BatteryFaultsData
     bool can12VSupplyLow;
     bool contactorStuck;
     bool cmuDetectedExtraCellPresent;
+
+    /*
+    BatteryFaultsData()
+        : internalCommFault(true)
+        , internalConversionFault(false)
+        , weakCellFault(true)
+        , lowCellVoltageFault(false)
+        , openWiringFault(true)
+        , currentSensorFault(false)
+        , packVoltageSensorFault(true)
+        , weakPackFault(false)
+        , voltageRedundancyFault(true)
+        , fanMonitorFault(false)
+        , thermistorFault(true)
+        , canbusCommsFault(false)
+        , highVoltageIsolationFault(false)
+        , dclReducedLowSoc(true)
+        , dclReducedLowTemp(false)
+        , dclReducedLowCellVoltage(true)
+        , dclReducedLowPackVoltage(false)
+        , dclCclReducedVoltageFailsafe(true)
+        , dclCclReducedCommsFailsafe(false)
+        , cclReducedHighSoc(true)
+        , cclReducedHighCellResist(false)
+        , cclReducedTemperature(true)
+        , cclReducedHighCellVoltage(false)
+        , cclReducelACLimit(true)
+    {}
+
+    bool internalCommFault;
+    bool internalConversionFault;
+    bool weakCellFault;
+    bool lowCellVoltageFault;
+    bool openWiringFault;
+    bool currentSensorFault;
+    bool packVoltageSensorFault;
+    bool weakPackFault;
+    bool voltageRedundancyFault;
+    bool fanMonitorFault;
+    bool thermistorFault;
+    bool canbusCommsFault;
+    bool highVoltageIsolationFault;
+    bool dclReducedLowSoc;
+    bool dclReducedLowTemp;
+    bool dclReducedLowCellVoltage;
+    bool dclReducedLowPackVoltage;
+    bool dclCclReducedVoltageFailsafe;
+    bool dclCclReducedCommsFailsafe;
+    bool cclReducedHighSoc;
+    bool cclReducedHighCellResist;
+    bool cclReducedTemperature;
+    bool cclReducedHighCellVoltage;
+    bool cclReducelACLimit;
+  */
 };
